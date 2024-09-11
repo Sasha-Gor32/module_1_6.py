e1e848e5bd38fe8bfccfa9b93a94090880a7b3cf
Sasha-Gor32 <sasha051505@gmail.com>
2024-09-11 18:32:40 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,10 @@
+my_dict = {'Илья': 2004, 'Вика': 2001}
+my_dict['Маша'] = 2004
+my_dict['Саша'] = 2006
+del my_dict['Вика']
+
+print(my_dict)
+my_set = {23,23, 'Арбуз','Арбуз', 4.13, 4.13}
+my_set.update ({'Малина', 36})
+print(my_set.remove (4.13))
+print(my_set)
